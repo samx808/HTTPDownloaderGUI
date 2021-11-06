fabfdf9a6b5e84edcf039f2a70013306fc15d050
samx808 <84256955+samx808@users.noreply.github.com>
2021-11-06 14:31:28 +1100

    Enter key functionality
    Also change window size handling

--- src/GUI.java
+++ src/GUI.java
@@ -12,6 +12,10 @@ import javax.swing.JOptionPane;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.TraverseEvent;
+import org.eclipse.swt.events.TraverseListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
@@ -32,7 +36,7 @@ public class GUI {
 	public GUI() {
 		Display display = Display.getDefault();
 		Shell shell = new Shell();
-		shell.setSize(900, 345);
+		shell.setSize(700, 110);
 		shell.setText("HTTP Downloader");
 		shell.addDisposeListener(new DisposeListener() {
 			
@@ -42,39 +46,75 @@ public class GUI {
 				
 			}
 		});
+		shell.setLayout(new GridLayout(3, false));
 		
 		Label lblUrlToDownload = new Label(shell, SWT.NONE);
 		lblUrlToDownload.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
-		lblUrlToDownload.setBounds(10, 200, 171, 21);
 		lblUrlToDownload.setText("URL to Download from:");
 		
 		textURL = new Text(shell, SWT.BORDER);
-		textURL.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
-		textURL.setBounds(187, 200, 650, 32);
-		
-		Button btnDownload = new Button(shell, SWT.NONE);
-		btnDownload.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
-		btnDownload.setBounds(743, 259, 94, 31);
-		btnDownload.setText("Download");
+		GridData gd_textURL = new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1);
+		gd_textURL.widthHint = 153;
+		textURL.setLayoutData(gd_textURL);
+		textURL.setFont(SWTResourceManager.getFont("Segoe UI", 11, SWT.NORMAL));
+		textURL.addTraverseListener(new TraverseListener()
+		  {
+		    @Override
+		    public void keyTraversed(final TraverseEvent event)
+		    {
+		      if (event.detail == SWT.TRAVERSE_RETURN)
+		        { 
+		    	  try {
+	        		  URLStr = textURL.getText();
+	        		  LocalDirStr = textLocalDir.getText(); 
+					downloader();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+					}		        
+		        }
+		    }
+		  });
 		
 		Label lbllocaldir = new Label(shell, SWT.NONE);
 		lbllocaldir.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
-		lbllocaldir.setBounds(10, 261, 171, 21);
 		lbllocaldir.setText("File Name to save:");
 		
 		textLocalDir = new Text(shell, SWT.BORDER);
-		textLocalDir.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
-		textLocalDir.setBounds(187, 258, 550, 32);
+		textLocalDir.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		textLocalDir.setFont(SWTResourceManager.getFont("Segoe UI", 11, SWT.NORMAL));
+		textLocalDir.addTraverseListener(new TraverseListener()
+		  {
+		    @Override
+		    public void keyTraversed(final TraverseEvent event)
+		    {
+		      if (event.detail == SWT.TRAVERSE_RETURN)
+		        { 
+		    	  try {
+	        		  URLStr = textURL.getText();
+	        		  LocalDirStr = textLocalDir.getText(); 
+					downloader();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+					}		        
+		        }
+		    }
+		  });
+		
+		Button btnDownload = new Button(shell, SWT.NONE);
+		GridData gd_btnDownload = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_btnDownload.widthHint = 97;
+		btnDownload.setLayoutData(gd_btnDownload);
+		btnDownload.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
+		btnDownload.setText("Download");
 		btnDownload.addListener(SWT.Selection, new Listener() {
 		      public void handleEvent(Event e) {
 		          switch (e.type) {
-		          case SWT.Selection:
+		          case SWT.Selection: 
 		        	  try {
 		        		  URLStr = textURL.getText();
 		        		  LocalDirStr = textLocalDir.getText(); 
 						downloader();
 					} catch (IOException e1) {
-						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
 		          }
